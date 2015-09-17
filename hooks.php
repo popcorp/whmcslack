2f177b8ba7b95fc9f3d49f24ab3afa2005511cca
PunKeel <punkeel@me.com>
2015-09-18 01:10:10 +0200

    Let it go
    Make it less noti and more Slack.

--- hooks.php
+++ hooks.php
@@ -1,52 +1,85 @@
 <?php
 
-function noti_ClientAdd($vars) {
-  global $customadminpath, $CONFIG;
-  $application_key = mysql_fetch_array( select_query('tbladdonmodules', 'value', array('module' => 'noti', 'setting' => 'key') ), MYSQL_ASSOC );
-  $administrators  = full_query("SELECT `access_token` FROM `tblnoti` WHERE `permissions` LIKE '%new_client%'");
-  while($administrator = mysql_fetch_array( $administrators, MYSQL_ASSOC )){
-    $noti[] = $administrator['access_token'];
-  }
-  $noti = implode($noti, ',');
-  curlCall("http://notiapp.com/api/v1/bulk",array('app' => $application_key['value'], 'users' => $noti, 'notification[title]' => 'New WHMCS Client', 'notification[text]' => 'A new client has signed up!', 'notification[sound]' => 'fanfare', 'notification[url]' => $CONFIG['SystemURL'].'/'.$customadminpath.'/clientssummary.php?userid='.$vars['userid']));
-}
 
-function noti_InvoicePaid($vars) {
-    global $customadminpath, $CONFIG;
-    $application_key = mysql_fetch_array( select_query('tbladdonmodules', 'value', array('module' => 'noti', 'setting' => 'key') ), MYSQL_ASSOC );
-    $administrators  = full_query("SELECT `access_token` FROM `tblnoti` WHERE `permissions` LIKE '%new_invoice%'");
-    while($administrator = mysql_fetch_array( $administrators, MYSQL_ASSOC )){
-      $noti[] = $administrator['access_token'];
+function whmcslack_getConfig()
+{
+    $q = select_query('tbladdonmodules', 'value', array('module' => 'whmcslack', 'setting' => 'webhook'));
+    if (mysql_num_rows($q) == 0)
+        return null;
+    $r = [];
+    while ($d = mysql_fetch_array($q)) {
+        $r[$d['setting']] = $d['value'];
     }
-    $noti = implode($noti, ',');
-    curlCall("http://notiapp.com/api/v1/bulk",array('app' => $application_key['value'], 'users' => $noti, 'notification[title]' => 'An invoice has just been paid', 'notification[text]' => 'Invoice #'.$vars['invoiceid'].' has been paid.', 'notification[sound]' => 'cash', 'notification[url]' => $CONFIG['SystemURL'].'/'.$customadminpath.'/invoices.php?action=edit&id='.$vars['invoiceid']));
 }
 
-function noti_TicketOpen($vars) {
-  global $customadminpath, $CONFIG;
-  $application_key = mysql_fetch_array( select_query('tbladdonmodules', 'value', array('module' => 'noti', 'setting' => 'key') ), MYSQL_ASSOC );
-  $administrators  = full_query("SELECT `access_token` FROM `tblnoti` WHERE `permissions` LIKE '%new_ticket%'");
-  while($administrator = mysql_fetch_array( $administrators, MYSQL_ASSOC )){
-    $noti[] = $administrator['access_token'];
-  }
-  $noti = implode($noti, ',');
-  curlCall("http://notiapp.com/api/v1/bulk",array('app' => $application_key['value'], 'users' => $noti, 'notification[title]' => 'A new ticket has arrived', 'notification[text]' => $vars['subject'].' (in '.$vars['deptname'].')', 'notification[sound]' => 'subtle1', 'notification[url]' => $CONFIG['SystemURL'].'/'.$customadminpath.'/supporttickets.php?action=viewticket&id='.$vars['ticketid']));
+function whmcslack_call($webhookUrl, $data)
+{
+    $payload = ['payload' => json_encode($data)];
+    $response = curlCall($webhookUrl, $payload);
 }
 
-function noti_TicketUserReply($vars) {
-  global $customadminpath, $CONFIG;
-  $application_key = mysql_fetch_array( select_query('tbladdonmodules', 'value', array('module' => 'noti', 'setting' => 'key') ), MYSQL_ASSOC );
-  $administrators  = full_query("SELECT `access_token` FROM `tblnoti` WHERE `permissions` LIKE '%new_update%'");
-  while($administrator = mysql_fetch_array( $administrators, MYSQL_ASSOC )){
-    $noti[] = $administrator['access_token'];
-  }
-  $noti = implode($noti, ',');
-  curlCall("http://notiapp.com/api/v1/bulk",array('app' => $application_key['value'], 'users' => $noti, 'notification[title]' => 'A ticket has been updated', 'notification[text]' => $vars['subject'].' (in '.$vars['deptname'].')', 'notification[sound]' => 'subtle1', 'notification[url]' => $CONFIG['SystemURL'].'/'.$customadminpath.'/supporttickets.php?action=viewticket&id='.$vars['ticketid']));
+function whmcslack_ClientAdd($vars)
+{
+    global $customadminpath, $CONFIG;
+    $conf = whmcslack_config();
+    if ($conf == null || empty($conf['webhook']))
+        return;
+    if (!$conf['new_client'])
+        return;
+    $url = $CONFIG['SystemURL'] . '/' . $customadminpath . '/clientssummary.php?userid=' . $vars['userid'];
+    $data = [];
+    $data['text'] = 'A new client has signed up! ' .
+        '<' . $url . '|Click here> for details!';
+    whmcslack_call($conf['webhook'], $data);
+}
+
+function whmcslack_InvoicePaid($vars)
+{
+    global $customadminpath, $CONFIG;
+    $conf = whmcslack_config();
+    if ($conf == null || empty($conf['webhook']))
+        return;
+    if (!$conf['new_invoice'])
+        return;
+    $url = $CONFIG['SystemURL'] . '/' . $customadminpath . '/invoices.php?action=edit&id=' . $vars['invoiceid'];
+    $data = [];
+    $data['text'] = 'Invoice ' . $vars['invoiceid'] . ' has just been paid! ' .
+        '<' . $url . '|Click here> for details!';
+    whmcslack_call($conf['webhook'], $data);
+}
+
+function whmcslack_TicketOpen($vars)
+{
+    global $customadminpath, $CONFIG;
+    $conf = whmcslack_config();
+    if ($conf == null || empty($conf['webhook']))
+        return;
+    if (!$conf['new_ticket'])
+        return;
+    $url = $CONFIG['SystemURL'] . '/' . $customadminpath . '/supporttickets.php?action=viewticket&id=' . $vars['ticketid'];
+    $data = [];
+    $data['text'] = 'New ticket: _' . $vars['subject'] . '_ (in ' . $vars['deptname'] . ') ' .
+        '<' . $url . '|Click here> for details!';
+    whmcslack_call($conf['webhook'], $data);
+}
+
+function whmcslack_TicketUserReply($vars)
+{
+    global $customadminpath, $CONFIG;
+    $conf = whmcslack_config();
+    if ($conf == null || empty($conf['webhook']))
+        return;
+    if (!$conf['new_update'])
+        return;
+    $url = $CONFIG['SystemURL'] . '/' . $customadminpath . '/supporttickets.php?action=viewticket&id=' . $vars['ticketid'];
+    $data = [];
+    $data['text'] = 'Ticket #' . $vars['ticketid'] . ' has been updated _' . $vars['subject'] . '_ (in ' . $vars['deptname'] . ') ' .
+        '<' . $url . '|Click here> for details!';
+    whmcslack_call($conf['webhook'], $data);
 }
 
 
-
-add_hook("ClientAdd",1,"noti_ClientAdd");
-add_hook("InvoicePaid",1,"noti_InvoicePaid");
-add_hook("TicketOpen",1,"noti_TicketOpen");
-add_hook("TicketUserReply",1,"noti_TicketUserReply");
+add_hook("ClientAdd", 1, "whmcslack_ClientAdd");
+add_hook("InvoicePaid", 1, "whmcslack_InvoicePaid");
+add_hook("TicketOpen", 1, "whmcslack_TicketOpen");
+add_hook("TicketUserReply", 1, "whmcslack_TicketUserReply");
---
+++ whmcslack.php
@@ -0,0 +1,37 @@
+<?php
+
+if (!defined('WHMCS'))
+    die('This file cannot be accessed directly');
+
+function whmcslack_config()
+{
+    $configarray = [
+        'name' => 'WHMCSlack',
+        'description' => 'Receive a Slack notification for major event',
+        'version' => '1.0',
+        'author' => 'PunKeel',
+        'language' => 'english',
+        'fields' => [
+            'webhook' => ['FriendlyName' => 'Slack Webhook', 'Type' => 'text', 'Size' => '80', 'Description' => '', 'Default' => 'https://hooks.slack.com/services/T012345AC/B0AT00A0A/...'],
+            'new_client' => ['FriendlyName' => 'Alert New Client', 'Type' => 'yesno', 'Size' => '25',
+                'Description' => ''],
+            'new_invoice' => ['FriendlyName' => 'Alert Invoice Paid', 'Type' => 'yesno', 'Size' => '25',
+                'Description' => ''],
+            'new_ticket' => ['FriendlyName' => 'Alert New Ticket', 'Type' => 'yesno', 'Size' => '25',
+                'Description' => ''],
+            'new_update' => ['FriendlyName' => 'Alert Ticket Update', 'Type' => 'yesno', 'Size' => '25',
+                'Description' => ''],
+        ]
+    ];
+    return $configarray;
+}
+
+function whmcslack_activate()
+{
+    return array('status' => 'info', 'description' => 'WHMCSlack is now enabled!');
+}
+
+function whmcslack_deactivate()
+{
+    return array('status' => 'info', 'description' => 'WHMCSlack is now enabled!');
+}
